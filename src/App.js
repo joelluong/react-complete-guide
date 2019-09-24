25eced52155cb5a4e9fd6ac9d2b87bfa7b855613
Joel Luong <Joel@loop.local>
2019-09-24 13:18:34 +1000

    useState Hook 1

--- src/App.js
+++ src/App.js
@@ -1,46 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Person from './components/Person/Person';
 
-class App extends Component {
-  state = {
-    persons: [
-      { name: 'Joel', age: 25},
-      { name: 'Manu', age: 28},
-      { name: 'Cindy', age: 26}
-    ],
-    otherState: 'some other value'
-  }
+//class App extends Component 
+const app = props => 
+{
+  // default way to create state in class base component
+  return (
+    <div className="App">
+      <h1>Hi, I am a React app</h1>
+      <p>This is really working </p>
+      <button onClick={this.switchNameHandler}>Switch Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobbies: Soccer</Person>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+    </div>
+  );
+ 
 
-  switchNameHandler = () => {
-    // console.log('was clicked!');
-    // DONT DO THIS: this.state.persons[0].name = 'Joel'
-    this.setState( 
-      {
-        persons: [
-          { name: 'Joel Luong', age: 25},
-          { name: 'Manu', age: 28},
-          { name: 'Cindy', age: 24}
-        ]
-      }
-     )
-  }
-
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I am a React app</h1>
-        <p>This is really working </p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobbies: Soccer</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-      </div>
-    );
-    //return React.createElement('div', null, 'h1', 'Hello Joel')
-  }
+  // switchNameHandler = () => {
+  //   // console.log('was clicked!');
+  //   // DONT DO THIS: this.state.persons[0].name = 'Joel'
+  //   this.setState( 
+  //     {
+  //       persons: [
+  //         { name: 'Joel Luong', age: 25},
+  //         { name: 'Manu', age: 28},
+  //         { name: 'Cindy', age: 24}
+  //       ]
+  //     }
+  //    )
+  // }
 }
 
-export default App;
+export default app;
+
+
+state = {
+  persons: [
+    { name: 'Joel', age: 25},
+    { name: 'Manu', age: 28},
+    { name: 'Cindy', age: 26}
+  ],
+  otherState: 'some other value'
+}
