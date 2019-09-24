ced8afe27b542f128f5a83d0fb7aae61c147b603
Joel Luong <Joel@loop.local>
2019-09-24 15:05:21 +1000

    Revert change from userState React Hooks

--- src/App.js
+++ src/App.js
@@ -3,45 +3,48 @@ import React, { Component, useState } from 'react';
 import './App.css';
 import Person from './components/Person/Person';
 
-//class App extends Component 
-const app = props => 
+class App extends Component 
 {
+  state = {
+    persons: [
+      { name: 'Joel', age: 25},
+      { name: 'Manu', age: 28},
+      { name: 'Cindy', age: 26}
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = () => {
+    // console.log('was clicked!');
+    // DONT DO THIS: this.state.persons[0].name = 'Joel'
+    this.setState( 
+      {
+        persons: [
+          { name: 'Joel Luong', age: 25},
+          { name: 'Manu', age: 28},
+          { name: 'Cindy', age: 24}
+        ]
+      }
+     )
+  }
+
+
   // default way to create state in class base component
-  return (
-    <div className="App">
-      <h1>Hi, I am a React app</h1>
-      <p>This is really working </p>
-      <button onClick={this.switchNameHandler}>Switch Name</button>
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobbies: Soccer</Person>
-      <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I am a React app</h1>
+        <p>This is really working </p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobbies: Soccer</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+  }
  
 
-  // switchNameHandler = () => {
-  //   // console.log('was clicked!');
-  //   // DONT DO THIS: this.state.persons[0].name = 'Joel'
-  //   this.setState( 
-  //     {
-  //       persons: [
-  //         { name: 'Joel Luong', age: 25},
-  //         { name: 'Manu', age: 28},
-  //         { name: 'Cindy', age: 24}
-  //       ]
-  //     }
-  //    )
-  // }
+  
 }
 
-export default app;
-
-
-state = {
-  persons: [
-    { name: 'Joel', age: 25},
-    { name: 'Manu', age: 28},
-    { name: 'Cindy', age: 26}
-  ],
-  otherState: 'some other value'
-}
+export default App;
